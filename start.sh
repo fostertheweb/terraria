dbc990998152660800100be75804e684275a30bf
Jonathan Foster <jxf9099@rit.edu>
2018-08-21 01:26:21 +0000

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -it --name terraria \
-  -p 7777:7777 \
-  -v tsworlds:/etc/terraria:rw \
-  -v tsworlds:/usr/terraria/worlds:rw \
-  -v tsworlds:/var/log/terraria:rw \
-  fostertheweb/terraria
